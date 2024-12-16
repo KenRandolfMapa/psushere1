35898814c30c02f77ceeb1904c113dac9b81c795
KenRandolfMapa <2011880166@psu.palawan.edu.ph>
2024-12-16 18:11:12 +0800

    add all feature

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -1,22 +1,46 @@
-"""
-URL configuration for projectsite project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from studentorg.views import HomePageView, OrganizationList, OrganizationCreateView, OrganizationUpdateView, OrganizationDeleteView, OrgMemberList , OrgMemberCreateView, OrgMemberUpdateView, OrgMemberDeleteView,  StudentList , StudentCreateView, StudentUpdateView, StudentDeleteView,  CollegeList , CollegeCreateView, CollegeUpdateView, CollegeDeleteView, ProgramList , ProgramCreateView, ProgramUpdateView, ProgramDeleteView, ChartView, StudentCountByProgram, OrganizationGraphData, chart_colleges
+from studentorg import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.HomePageView.as_view(), name='home'),
+
+    path('dashboard_chart', ChartView.as_view(), name='dashboard-chart'),
+    path('student-count-by-program/', StudentCountByProgram, name='student-count-by-program'),
+    path('organization-graph-data/', OrganizationGraphData, name='organization-graph-data'),
+    path('pie-chart/students/', views.chart_students, name='pie_chart_students'),
+    path('pie-chart/org-members/', views.chart_org_members, name='pie_chart_org_members'),
+    path('pie-chart/colleges/', chart_colleges, name='pie_chart_colleges'),
+
+    path('organization-list/', OrganizationList.as_view(), name='organization-list'),
+    path('organization-list/add', OrganizationCreateView.as_view(), name='organization-add'),
+    path('organization-list/<pk>', OrganizationUpdateView.as_view(), name='organization-update'),
+    path('organization-list/<pk>/delete', OrganizationDeleteView.as_view(), name='organization-delete'),
+
+    path('orgmember_list', OrgMemberList.as_view(), name='orgmember-list'),
+    path('orgmember_list/add', OrgMemberCreateView.as_view(), name='orgmember-add'),
+    path('orgmember_list/<pk>', OrgMemberUpdateView.as_view(), name='orgmember-update'),
+    path('orgmember_list/<pk>/delete',OrgMemberDeleteView.as_view(), name='orgmember-delete'),
+
+    path('student_list', StudentList.as_view(), name='student-list'),
+    path('student_list/add', StudentCreateView.as_view(), name='student-add'),
+    path('student_list/<pk>', StudentUpdateView.as_view(), name='student-update'),
+    path('student_list/<pk>/delete', StudentDeleteView.as_view(), name='student-delete'),
+
+    path('college_list', CollegeList.as_view(), name='college-list'),
+    path('college_list/add', CollegeCreateView.as_view(), name='college-add'),
+    path('college_list/<pk>', CollegeUpdateView.as_view(), name='college-update'),
+    path('college_list/delete/<pk>', CollegeDeleteView.as_view(), name='college-delete'),
+
+    path('program_list', ProgramList.as_view(), name='program-list'),
+    path('program_list/add', ProgramCreateView.as_view(), name='program-add'),
+    path('program_list/<pk>', ProgramUpdateView.as_view(), name='program-update'),
+    path('program_list/<pk>/delete', ProgramDeleteView.as_view(), name='program-delete'),
+
+    re_path(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    re_path(r'^Logout/$', auth_views.LogoutView.as_view(), name='logout'),
+
 ]
--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -1,3 +1,289 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+from studentorg.models import Organization, OrgMember, Student, College, Program
+from studentorg.forms import OrganizationForm, OrgMemberForm, StudentForm, CollegeForm, ProgramForm
+
+from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+
+from typing import Any
+from django.db.models.query import QuerySet
+from django.db.models import Q
+
+from django.db import connection
+from django.http import JsonResponse
+from django.db.models.functions import ExtractMonth
+
+from django.db.models import Count
+from datetime import datetime
+
+from django.views.generic import View
+
+from django.db import models
+
+class ChartView(ListView):
+    template_name = 'chart.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+    def get_queryset(self, *args, **kwargs):
+        pass
+
+
+@method_decorator(login_required, name='dispatch')
+class HomePageView(ListView):
+    model = Organization
+    context_object_name = 'home'
+    template_name = "home.html"
+
+
+class ChartView(ListView):
+    template_name = 'chart.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+    def get_queryset(self, *args, **kwargs):
+        pass
+
+
+def StudentCountByProgram(request):
+    # Fetching student counts per program
+    program_counts = Student.objects.values('program__prog_name').annotate(count=Count('program')).order_by('-count')
+
+    # Extracting labels (program names) and counts
+    labels = [item['program__prog_name'] for item in program_counts]
+    counts = [item['count'] for item in program_counts]
+
+    data = {
+        'labels': labels,
+        'counts': counts,
+    }
+    return JsonResponse(data)
+
+
+def OrganizationGraphData(request):
+    # Your code to fetch organization graph data from the database or any other source
+    # Example data for demonstration purposes
+    labels = ['X', 'Y', 'Z']
+    counts = [50, 70, 30]
+    
+    data = {
+        'labels': labels,
+        'counts': counts,
+    }
+    
+    return JsonResponse(data)
+
+
+def chart_students(request):
+    program_counts = Program.objects.annotate(student_count=models.Count('student'))
+    labels = [program.prog_name for program in program_counts]
+    counts = [program.student_count for program in program_counts]
+    return JsonResponse({'labels': labels, 'counts': counts})
+
+
+def chart_org_members(request):
+    org_counts = Organization.objects.annotate(member_count=models.Count('orgmember'))
+    labels = [org.name for org in org_counts]
+    counts = [org.member_count for org in org_counts]
+    return JsonResponse({'labels': labels, 'counts': counts})
+
+
+def chart_colleges(request):
+    # Query the database to get data for the college pie chart
+    # For example, let's say you want to count the number of students per college
+    college_counts = College.objects.annotate(student_count=models.Count('program__student'))
+    labels = [college.college_name for college in college_counts]
+    counts = [college.student_count for college in college_counts]
+    
+    # Return the data as JSON response with a unique name
+    return JsonResponse({'college_labels': labels, 'college_counts': counts})
+
 
 # Create your views here.
+class OrganizationList(ListView):
+    model = Organization
+    context_object_name = 'organization'
+    template_name = 'organization/org_list.html'
+    paginate_by = 5
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super(OrganizationList, self).get_queryset(*args, **kwargs)
+        if self.request.GET.get("q") != None:
+            query = self.request.GET.get('q')
+            qs = qs.filter(Q(name__icontains=query) |
+                           Q(description__icontains=query) |
+                           Q(college__college_name__icontains=query))
+        return qs
+
+
+class OrganizationCreateView(CreateView):
+    model = Organization
+    form_class = OrganizationForm
+    template_name = 'organization/org_add.html'
+    success_url = reverse_lazy('organization-list')
+
+
+class OrganizationUpdateView(UpdateView):
+    model = Organization
+    form_class = OrganizationForm
+    template_name = 'organization/org_edit.html'
+    success_url = reverse_lazy('organization-list')
+
+
+class OrganizationDeleteView(DeleteView):
+    model = Organization
+    template_name = 'organization/org_del.html'
+    success_url = reverse_lazy('organization-list')
+
+
+class OrgMemberList(ListView):
+    model = OrgMember
+    context_object_name = 'orgmember'
+    template_name = 'org_member/orgmember_list.html'
+    paginate_by = 10
+    def get_queryset(self, *args, **kwargs):
+        qs = super(OrgMemberList, self).get_queryset(*args, **kwargs)
+        if self.request.GET.get("q") != None:
+            query = self.request.GET.get('q')
+            qs = qs.filter(Q(organization__name__icontains=query))
+        return qs
+
+
+class OrgMemberCreateView(CreateView):
+    model = OrgMember
+    form_class = OrgMemberForm
+    template_name = 'org_member/orgmember_add.html'
+    success_url = reverse_lazy('orgmember-list')
+
+
+class OrgMemberUpdateView(UpdateView):
+    model = OrgMember
+    form_class = OrgMemberForm
+    template_name = 'org_member/orgmember_edit.html'
+    success_url = reverse_lazy('orgmember-list')
+
+
+class OrgMemberDeleteView(DeleteView):
+    model = OrgMember
+    form_class = OrgMemberForm
+    template_name = 'org_member/orgmember_del.html'
+    success_url = reverse_lazy('orgmember-list')
+
+
+class StudentList(ListView):
+    model = Student
+    context_object_name = 'student'
+    template_name = 'student/stud_list.html'
+    paginate_by = 10
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super(StudentList, self).get_queryset(*args, **kwargs)
+        if self.request.GET.get("q") != None:
+            query = self.request.GET.get('q')
+            qs = qs.filter(Q(firstname__icontains=query) |
+                        Q (lastname__icontains=query) |
+                        Q (middlename__icontains=query) |
+                        Q (student_id__icontains=query))
+        return qs
+
+
+class StudentCreateView(CreateView):
+    model = Student
+    form_class = StudentForm
+    template_name = 'student/stud_add.html'
+    success_url = reverse_lazy('student-list')
+
+
+class StudentUpdateView(UpdateView):
+    model = Student
+    form_class = StudentForm
+    template_name = 'student/stud_edit.html'
+    success_url = reverse_lazy('student-list')
+
+
+class StudentDeleteView(DeleteView):
+    model = Student
+    form_class = StudentForm
+    template_name = 'student/stud_del.html'
+    success_url = reverse_lazy('student-list')
+
+
+class CollegeList(ListView):
+    model = College
+    context_object_name = 'college'
+    template_name = 'college/college_list.html'
+    paginate_by = 10
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super(CollegeList, self).get_queryset(*args, **kwargs)
+        if self.request.GET.get("q") != None:
+            query = self.request.GET.get('q')
+            qs = qs.filter(Q(college_name__icontains=query))
+        return qs
+
+
+class CollegeCreateView(CreateView):
+    model = College
+    form_class = CollegeForm
+    template_name = 'college/college_add.html'
+    success_url = reverse_lazy('college-list')
+
+
+class CollegeUpdateView(UpdateView):
+    model = College
+    form_class = CollegeForm
+    template_name = 'college/college_edit.html'
+    success_url = reverse_lazy('college-list')
+
+
+class CollegeDeleteView(DeleteView):
+    model = College
+    form_class = CollegeForm
+    template_name = 'college/college_del.html'
+    success_url = reverse_lazy('college-list')
+
+
+class ProgramList(ListView):
+    model = Program
+    context_object_name = 'program'
+    template_name = 'program/program_list.html'
+    paginate_by = 10
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super(ProgramList, self).get_queryset(*args, **kwargs)
+        if self.request.GET.get("q") != None:
+            query = self.request.GET.get('q')
+            qs = qs.filter(Q(prog_name__icontains=query) |
+                        Q (college__college_name__icontains=query))
+        return qs
+
+
+class ProgramCreateView(CreateView):
+    model = Program
+    form_class = ProgramForm
+    template_name = 'program/program_add.html'
+    success_url = reverse_lazy('program-list')
+
+
+class ProgramUpdateView(UpdateView):
+    model = Program
+    form_class = ProgramForm
+    template_name = 'program/program_edit.html'
+    success_url = reverse_lazy('program-list')
+
+
+class ProgramDeleteView(DeleteView):
+    model = Program
+    form_class = ProgramForm
+    template_name = 'program/program_del.html'
+    success_url = reverse_lazy('program-list')
+
+
